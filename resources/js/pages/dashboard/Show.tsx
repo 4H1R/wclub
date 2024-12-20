79c5cba211dddc9fbd9e437af2a6f2130e4c440f
Ali Alizadeh <ghostrockz3d@gmail.com>
2024-12-21 02:27:48 +0330

    add list scores

--- resources/js/pages/dashboard/Show.tsx
+++ resources/js/pages/dashboard/Show.tsx
@@ -11,6 +11,16 @@ export default function Show() {
   const { auth } = usePage().props;
   const showTooltip = useShowTooltip();
 
+  const scores = [
+    `${digitsEnToFa(20)} امتیاز بابت خواندن همه رویداد ها`,
+    `${digitsEnToFa(20)} امتیاز بابت برنده شدن تاس سی`,
+    `${digitsEnToFa(20)} امتیاز بابت چرخوندن گردونه`,
+    `${digitsEnToFa(20)} امتیاز بابت شرکت در رویداد`,
+    `${digitsEnToFa(40)} امتیاز بابت ورود به باشگاه بانوان به مدت ${digitsEnToFa(30)} روز`,
+    `${digitsEnToFa(30)} امتیاز بابت دیدن دوره`,
+    `مجموع ${digitsEnToFa(155)} امتیاز`,
+  ];
+
   useEffect(() => {
     if (route().params['auth_was_successful']) {
       toast.success('شما با موفقیت وارد حساب کاربری خود شدید.');
@@ -35,10 +45,19 @@ export default function Show() {
         >
           <div className="badge badge-lg flex items-center justify-center gap-2 bg-yellow-600 text-white">
             <HiStar className="size-4" />
-            <span className="font-fa-display">
-              {digitsEnToFa(addCommas(auth.user?.score ?? 0))}
-            </span>
+            <span className="font-fa-display">{digitsEnToFa(addCommas(150))}</span>
           </div>
+        </div>
+      </div>
+      <div className="divider" />
+      <div className="card card-bordered bg-base-200 md:max-w-[50%]">
+        <div className="card-body">
+          <h3 className="card-title">لیست امتیاز ها</h3>
+          <ul className="mt-4 list-inside list-disc">
+            {scores.map((text, i) => (
+              <li key={i}>{text}</li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
