49c2b2745cf0a4b3fafd4b1cd78f4675432b5572
Ali Alizadeh <ghostrockz3d@gmail.com>
2025-03-24 16:46:13 +0330

    wip

--- app/Services/MellatService.php
+++ app/Services/MellatService.php
@@ -2,47 +2,45 @@
 
 namespace App\Services;
 
-use Exception;
-use SoapClient;
-
 class MellatService
 {
     const PAY_URL = 'https://bpm.shaparak.ir/pgwchannel/startpay.mellat';
 
     public function sendPaymentRequest(int $amount, int $orderId, int $payerId): array
     {
-        $data = [
-            'terminalId' => config('services.mellat.terminal_id'),
-            'userName' => config('services.mellat.username'),
-            'userPassword' => config('services.mellat.password'),
-            'orderId' => $orderId,
-            'amount' => $amount,
-            'localDate' => date('Ymd'),
-            'localTime' => date('Gis'),
-            'additionalData' => '',
-            'callBackUrl' => route('mellat.callback'),
-            'payerId' => $payerId,
-        ];
+        return [];
+        // $data = [
+        //     'terminalId' => config('services.mellat.terminal_id'),
+        //     'userName' => config('services.mellat.username'),
+        //     'userPassword' => config('services.mellat.password'),
+        //     'orderId' => $orderId,
+        //     'amount' => $amount,
+        //     'localDate' => date('Ymd'),
+        //     'localTime' => date('Gis'),
+        //     'additionalData' => '',
+        //     'callBackUrl' => route('mellat.callback'),
+        //     'payerId' => $payerId,
+        // ];
 
-        $client = new SoapClient(config('services.mellat.wsdl_url'), [
-            'trace' => 1,
-            'exceptions' => true,
-            'soap_version' => SOAP_1_1,
-            'encoding' => 'UTF-8',
-        ]);
+        // $client = new SoapClient(config('services.mellat.wsdl_url'), [
+        //     'trace' => 1,
+        //     'exceptions' => true,
+        //     'soap_version' => SOAP_1_1,
+        //     'encoding' => 'UTF-8',
+        // ]);
 
-        $result = $client->bpPayRequest($data);
+        // $result = $client->bpPayRequest($data);
 
-        if (! $result) {
-            throw new Exception('Payment request failed');
-        }
+        // if (! $result) {
+        //     throw new Exception('Payment request failed');
+        // }
 
-        $exploded = explode(',', $result);
+        // $exploded = explode(',', $result);
 
-        if ($exploded[0] !== '0') {
-            return ['statusCode' => (int) $exploded[0], 'refId' => $exploded[1] ?? null];
-        }
+        // if ($exploded[0] !== '0') {
+        //     return ['statusCode' => (int) $exploded[0], 'refId' => $exploded[1] ?? null];
+        // }
 
-        return ['statusCode' => $exploded[0], 'refId' => $exploded[1]];
+        // return ['statusCode' => $exploded[0], 'refId' => $exploded[1]];
     }
 }
