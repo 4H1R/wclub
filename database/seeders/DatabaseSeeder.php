885a30e88d07f1eed64c5b58b466c527273c188d
Ali Alizadeh <ghostrockz3d@gmail.com>
2025-12-01 10:49:01 +0330

    wip

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,79 +29,79 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        collect(File::json('database/json/target_groups.json')['entities'])->each(function (array $targetGroup) {
-            TargetGroup::firstOrCreate($targetGroup);
-        });
+        // collect(File::json('database/json/target_groups.json')['entities'])->each(function (array $targetGroup) {
+        //     TargetGroup::firstOrCreate($targetGroup);
+        // });
 
         $this->call([
             PermissionSeeder::class,
             RoleSeeder::class,
-            UserSeeder::class,
-            SeriesSeeder::class,
+            // UserSeeder::class,
+            // SeriesSeeder::class,
         ]);
 
-        $firstUser = User::firstOrFail();
+        // $firstUser = User::firstOrFail();
 
-        ContactUs::factory(30)->create();
-        Coupon::factory(30)->create();
-        Topic::factory(10)->create();
+        // ContactUs::factory(30)->create();
+        // Coupon::factory(30)->create();
+        // Topic::factory(10)->create();
 
-        $rewardProgramCategories = Category::factory(10)->create(['model' => RewardProgram::class]);
-        RewardProgram::factory(50)
-            ->create()
-            ->each(function (RewardProgram $rewardProgram) use ($rewardProgramCategories) {
-                $rewardProgram->categories()->attach($rewardProgramCategories->random(rand(0, 5))->pluck('id'));
-                $rewardProgram->targetGroups()->attach(TargetGroup::all()->random(rand(1, 3))->pluck('id'));
-            });
+        // $rewardProgramCategories = Category::factory(10)->create(['model' => RewardProgram::class]);
+        // RewardProgram::factory(50)
+        //     ->create()
+        //     ->each(function (RewardProgram $rewardProgram) use ($rewardProgramCategories) {
+        //         $rewardProgram->categories()->attach($rewardProgramCategories->random(rand(0, 5))->pluck('id'));
+        //         $rewardProgram->targetGroups()->attach(TargetGroup::all()->random(rand(1, 3))->pluck('id'));
+        //     });
 
-        $eventProgramCategories = Category::factory(10)->create(['model' => EventProgram::class]);
-        EventProgram::factory(50)
-            ->for($firstUser)
-            ->create()
-            ->each(function (EventProgram $eventProgram) use ($eventProgramCategories) {
-                $eventProgram->categories()->attach($eventProgramCategories->random(rand(0, 5))->pluck('id'));
-                $eventProgram->targetGroups()->attach(TargetGroup::all()->random(rand(1, 3))->pluck('id'));
-                Faq::factory(random_int(0, 5))->for($eventProgram, 'model')->create();
-            });
+        // $eventProgramCategories = Category::factory(10)->create(['model' => EventProgram::class]);
+        // EventProgram::factory(50)
+        //     ->for($firstUser)
+        //     ->create()
+        //     ->each(function (EventProgram $eventProgram) use ($eventProgramCategories) {
+        //         $eventProgram->categories()->attach($eventProgramCategories->random(rand(0, 5))->pluck('id'));
+        //         $eventProgram->targetGroups()->attach(TargetGroup::all()->random(rand(1, 3))->pluck('id'));
+        //         Faq::factory(random_int(0, 5))->for($eventProgram, 'model')->create();
+        //     });
 
-        $contestsCategories = Category::factory(10)->create(['model' => Contest::class]);
-        Contest::factory(50)
-            ->create()
-            ->each(function (Contest $contest) use ($contestsCategories) {
-                $contest->categories()->attach($contestsCategories->random(rand(0, 5))->pluck('id'));
-                $contest->targetGroups()->attach(TargetGroup::all()->random(rand(1, 3))->pluck('id'));
-            });
+        // $contestsCategories = Category::factory(10)->create(['model' => Contest::class]);
+        // Contest::factory(50)
+        //     ->create()
+        //     ->each(function (Contest $contest) use ($contestsCategories) {
+        //         $contest->categories()->attach($contestsCategories->random(rand(0, 5))->pluck('id'));
+        //         $contest->targetGroups()->attach(TargetGroup::all()->random(rand(1, 3))->pluck('id'));
+        //     });
 
-        $newsCategories = Category::factory(10)->create(['model' => News::class]);
-        News::factory(50)
-            ->create()
-            ->each(function (News $news) use ($newsCategories) {
-                $news->categories()->attach($newsCategories->random(rand(0, 5))->pluck('id'));
-                $news->targetGroups()->attach(TargetGroup::all()->random(rand(1, 3))->pluck('id'));
-            });
+        // $newsCategories = Category::factory(10)->create(['model' => News::class]);
+        // News::factory(50)
+        //     ->create()
+        //     ->each(function (News $news) use ($newsCategories) {
+        //         $news->categories()->attach($newsCategories->random(rand(0, 5))->pluck('id'));
+        //         $news->targetGroups()->attach(TargetGroup::all()->random(rand(1, 3))->pluck('id'));
+        //     });
 
-        Garden::factory(50)->create();
+        // Garden::factory(50)->create();
 
-        $users = User::pluck('id');
-        $series = Series::pluck('id');
-        Order::factory(20)->sequence(fn () => ['user_id' => $users->random()])
-            ->create()
-            ->each(function (Order $order) use ($series) {
-                $series->random(random_int(1, 5))
-                    ->each(function ($series_id) use ($order) {
-                        OrderItem::factory(random_int(1, 5))->create([
-                            'order_id' => $order->id,
-                            'model_type' => Series::class,
-                            'model_id' => $series_id,
-                        ]);
-                    });
-                Transaction::factory(random_int(1, 5))->create([
-                    'order_id' => $order->id,
-                    'user_id' => $order->user_id,
-                ]);
-            });
+        // $users = User::pluck('id');
+        // $series = Series::pluck('id');
+        // Order::factory(20)->sequence(fn() => ['user_id' => $users->random()])
+        //     ->create()
+        //     ->each(function (Order $order) use ($series) {
+        //         $series->random(random_int(1, 5))
+        //             ->each(function ($series_id) use ($order) {
+        //                 OrderItem::factory(random_int(1, 5))->create([
+        //                     'order_id' => $order->id,
+        //                     'model_type' => Series::class,
+        //                     'model_id' => $series_id,
+        //                 ]);
+        //             });
+        //         Transaction::factory(random_int(1, 5))->create([
+        //             'order_id' => $order->id,
+        //             'user_id' => $order->user_id,
+        //         ]);
+        //     });
 
-        HnText::factory(50)->create();
-        HnImage::factory(50)->create();
+        // HnText::factory(50)->create();
+        // HnImage::factory(50)->create();
     }
 }
